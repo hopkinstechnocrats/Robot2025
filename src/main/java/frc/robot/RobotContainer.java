8cea85775b4fad0298821ad140dff55d9b025e74
AJAmelse <117790563+AJAmelse@users.noreply.github.com>
2025-01-21 20:47:39 -0600

    created auto chooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -11,6 +12,8 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -33,6 +36,8 @@ public class RobotContainer
     // The robot's subsystems and commands are defined here...
     private final SwerveSubsystem drivebase  = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
         "swerve"));
+
+    private final SendableChooser<Command> autoChooser;
 
     /**
     * Converts driver input into a field-relative ChassisSpeeds that is controlled by angular velocity.
@@ -81,6 +86,19 @@ public class RobotContainer
         configureBindings();
         DriverStation.silenceJoystickConnectionWarning(true);
         NamedCommands.registerCommand("test", Commands.print("I EXIST"));
+
+        boolean isCompetition = false;
+
+        // Build an auto chooser. This will use Commands.none() as the default option.
+        // As an example, this will only show autos that start with "comp" while at
+        // competition as defined by the programmer
+        autoChooser = AutoBuilder.buildAutoChooserWithOptionsModifier(
+            (stream) -> isCompetition
+            ? stream.filter(auto -> auto.getName().startsWith("comp"))
+            : stream
+        );
+
+        SmartDashboard.putData("Auto Chooser", autoChooser);
     }
 
     /**
@@ -143,7 +161,7 @@ public class RobotContainer
     */
     public Command getAutonomousCommand(){
         // An example command will be run in autonomous
-        return drivebase.getAutonomousCommand("New Auto");
+        return autoChooser.getSelected();
     }
 
     public void setMotorBrake(boolean brake){
