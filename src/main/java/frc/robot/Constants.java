97981130a64ec8458a2e41827d9428cfc2a106aa
Lily <136532458+Freezerat@users.noreply.github.com>
2025-02-23 15:11:59 -0600

    made shallow end effector have a slightly deeper angle.  made lower l2 height slightly higher to compensate

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -58,8 +58,8 @@ public final class Constants
     public static final double kD = 0;
     public static final double motorPowerLimit = .1; //percent of max 1 (DO NOT SET TO NEAR 1)
     public static final double rotationsPerRevolution = 16*2*2 /*gear ratio, 16:1 gear box and 2 2:1 belts*/;
-    public static final double LeftScore = 0.125; //position when scoring on left
-    public static final double RightScore = -0.125; //position when scoring on right
+    public static final double LeftScore = 0.16; //position when scoring on left
+    public static final double RightScore = -0.16; //position when scoring on right
     public static final double LeftScoreL4 = 0.2; //position when scoring on left
     public static final double RightScoreL4 = -0.2; //position when scoring on left
     public static final double Stowage = 0.0; //stowage position for driving
@@ -77,7 +77,7 @@ public final class Constants
     public static final double L2Height = 7.625 + 13; //position when scoring on L2 inches
     public static final double L3Height = 13.25 + 23; //position when scoring on L3 inches
     public static final double L4Height = 21.75 + 38 + 6; //position when scoring on L4 inches
-    public static final double L2HeightEnd = 10; //position when scoring on L2 inches
+    public static final double L2HeightEnd = 11; //position when scoring on L2 inches
     public static final double kS = 0;
     public static final double kG = 0.22;
     public static final double kV = 10.66;
