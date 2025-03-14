5630ecdd34c14f968a179e0fea351ef65df3b5f1
Otto-hopkinsrobotics <otto.baune@hopkinsrobotics.org>
2025-03-13 20:31:17 -0500

    Update Climber.java

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.Servo;
@@ -16,6 +17,14 @@ public class Climber extends SubsystemBase{
         climbMotor = new TalonFX(14);
         climbMotor.setNeutralMode(NeutralModeValue.Brake);
         climbMotor.setVoltage(4);
+
+        var slot0Configs = new TalonFXConfiguration().Slot0;
+        slot0Configs.kP = 2.4; // An error of 1 rotation results in 2.4 V output
+        slot0Configs.kI = 0; // no output for integrated error
+        slot0Configs.kD = 0.1; // A velocity of 1 rps results in 0.1 V output
+
+        climbMotor.getConfigurator().apply(slot0Configs);
+
     }
 
     public void extend(){
